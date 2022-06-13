2ffd5277414969db90631c7416c051e6abd68f30
Gabriel Linhares <glinharesb@gmail.com>
2022-06-13 18:44:58 -0300

    fix(scrapping): adjust puppeteer args

--- src/services/offers.service.ts
+++ src/services/offers.service.ts
@@ -68,7 +68,9 @@ export class OffersService {
 
     const OFFERS_URL = 'https://www.mercadolivre.com.br/ofertas'
 
-    const browser = await puppeteer.launch()
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
+    })
     const page = await browser.newPage()
 
     await page.setJavaScriptEnabled(false)
